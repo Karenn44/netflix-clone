c5dc43665ab41458e11ba3085b35ad91df6e7542
Karen Johnson <rkarenjohnson@gmail.com>
2024-08-06 21:13:27 +0530

    netflix

--- js/login.js
+++
@@ -1,7 +0,0 @@
-let btnEntrar = document.querySelector('#btnEntrar');
-btnEntrar.addEventListener('click', entrar);
-
-function entrar(e){
-    e.preventDefault();
-    window.location.href = "user.html";
-}
